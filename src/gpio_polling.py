4c20f8bb6d7e0c43f318d91a16b74569aaa741be
iamhwany <ydh6335@gmail.com>
2026-04-24 13:55:55 +0900

    Add files via upload

--- src/gpio_polling.py
+++ src/gpio_polling.py
@@ -1,59 +1,69 @@
-import os
-os.system("sudo pigpiod")
-
-import pigpio
+from gpiozero import Button, LED
 import time
-import sys
 
 PIN_BTN  = 14
 PIN_LEDR = 16
 PIN_LEDG = 20
 PIN_LEDB = 21
 
-LOOP_PERIOD_MS = 2000 
+LOOP_PERIOD_MS = 2000
 
-led_color = 0
+# Start at state 7 (111 in binary), which matches the 'Off' state in your image
+led_val = 7
 
-def change_color(pi):
-    global led_color
+# Setup LEDs.
+# We use default active_high=True so that .value = 1 outputs HIGH (LED OFF)
+# and .value = 0 outputs LOW (LED ON). This perfectly matches your (111) to (000) logic.
+led_r = LED(PIN_LEDR)
+led_g = LED(PIN_LEDG)
+led_b = LED(PIN_LEDB)
+
+# Initial state (111 -> Off)
+led_r.value = 1
+led_g.value = 1
+led_b.value = 1
+
+# Button Setup: Internal pull-up
+btn = Button(PIN_BTN, pull_up=True)
+
+def change_color():
+    """Function to change LED colors based on current state (Countdown 7 to 0)"""
+    global led_val
 
     ################ Write Codes From Here ################
-    # Change colors
+
     #######################################################
+
+    # Console output (color information)
+    print(f"[Polling] LED Color changed to {led_r.value}{led_g.value}{led_b.value}")
 
 
 if __name__ == "__main__":
-    pi = pigpio.pi()
-    if not pi.connected:
-        print("pigpio demon error!", file=sys.stderr)
-        sys.exit(1)
-
-    pi.set_mode(PIN_BTN,  pigpio.INPUT)
-    pi.set_pull_up_down(PIN_BTN, pigpio.PUD_UP)
-
-    pi.set_mode(PIN_LEDR, pigpio.OUTPUT)
-    pi.set_mode(PIN_LEDG, pigpio.OUTPUT)
-    pi.set_mode(PIN_LEDB, pigpio.OUTPUT)
-
-    pi.write(PIN_LEDR, 1)
-    pi.write(PIN_LEDG, 1)
-    pi.write(PIN_LEDB, 1)
-
     print("!Polling!")
     count = 0
     try:
         while True:
             start = time.time()
-            btn = pi.read(PIN_BTN)
-
             print(f"Current seconds: {count:4d} [s]")
             count += 2
 
-            if btn == 0:
-                led_state = change_color(pi)
+            # Polling: Check the button state at this exact moment
+            # is_pressed returns True if the button is currently held down
+            if btn.is_pressed:
+                change_color()
 
             end = time.time()
-            time.sleep((LOOP_PERIOD_MS/1000) - (end - start))
+
+            # Maintain loop period (compensating for execution time)
+            sleep_time = (LOOP_PERIOD_MS / 1000.0) - (end - start)
+            if sleep_time > 0:
+                time.sleep(sleep_time)
+
+    except KeyboardInterrupt:
+        print("\nProgram exiting...")
     finally:
-        pi.stop()
-        os.system("sudo killall pigpiod")
+        # Turn off the LEDs and clean up resources upon exit
+        led_r.close()
+        led_g.close()
+        led_b.close()
+        btn.close()
